f064427ed49ba88dac4f2820c4dc28976894cd87
PARK GA IN <84880886+PARKGAIN@users.noreply.github.com>
2022-08-25 15:49:05 +0900

    Update detail.js

---
+++ detail.js
@@ -0,0 +1,10 @@
+axios.PUT('/user', {
+    firstName: 'Fred',
+    lastName: 'Flintstone'
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
