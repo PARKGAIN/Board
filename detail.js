68defdcba704277a31dc894ce2d8b0618c6f26c3
PARK GA IN <84880886+PARKGAIN@users.noreply.github.com>
2022-09-03 15:14:04 +0900

    Update detail.js

--- detail.js
+++ detail.js
@@ -20,29 +20,3 @@ axios
   .catch((err) => {
         console.log(err)
   })
-
-axios.delete('/user?ID=12345')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
-//삭제, 수정 버튼 만들어서 함수로 호출하기
-
-axios
-  .delete(`/api/review/${reviewId}`)
-//성공
-  .then((res) => {
-        console.log(res)
-  })
-//실패
-  .catch((err) => {
-        console.log('err')
-  })
-
